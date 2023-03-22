5c31d0b769945e70b54dd5e8b0ed511723e4747f
VasVL <sobakagman.com@gmail.com>
2023-03-22 13:39:13 +0300

    Behind the Scenes 7. Add a DatabaseVideo Entity - Бахнули класс DatabaseVideo, который через Room будет сохраняться в БД - И ещё определили две функции-расширения: Одна преобразует список <DatabaseVideo> в список <Video>, другая Извлекает из контйнера, который к нам приходит из интернетов видео, и помещает в БД (по сути, преобразует List<NetworkVideo> в Array<DatabaseVideo>) - Итого у нас три класса для видео: первый работает с сетью, второй с базой данных, третий будет показываться на экране

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,7 +17,34 @@
 
 package com.example.android.devbyteviewer.database
 
-// TODO (01) Create the DatabaseEntities class, adding annotations for the class and the primary key.
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
 
-// TODO (02) Define extension function List<DatabaseVideo>.asDomainModel(), that returns
-// a list of <Video>.
+/**
+ * (01) Create the DatabaseEntities class, adding annotations for the class and the primary key.
+ * */
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
+
+/**
+ * (02) Define extension function List<DatabaseVideo>.asDomainModel(), that returns a list of <Video>.
+ */
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return this.map {
+        Video(
+            url = it.url,
+            updated = it.updated,
+            title = it.title,
+            description = it.description,
+            thumbnail = it.thumbnail
+        )
+    }
+}
