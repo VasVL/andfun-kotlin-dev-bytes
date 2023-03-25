3d3a559a7f7c9c7ac0e8721bc89a446dcacbdf9d
VasVL <sobakagman.com@gmail.com>
2023-03-25 13:21:04 +0300

    Behind the Scenes 10. Add the VideoDatabase - Создаём БД - Насчёт synchronized надо бы проверить, я помню, что в java через двойной одинаковый if проверял, а между ними синхронайзед ставил, но там нужно было переменную как volatile указывать (в котлине есть аннотация @volatile). Тогда не нужно было бы каждый раз блокировать БД при получении инстанса

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,11 +17,15 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Dao
 interface VideoDao {
@@ -32,15 +36,40 @@ interface VideoDao {
     fun insertAll(vararg videos: DatabaseVideo)
 }
 
-// TODO (01) Create an abstract VideosDatabase class that extends RoomDatabase.
+/**
+ * (01) Create an abstract VideosDatabase class that extends RoomDatabase.
+ * (02) Annotate VideosDatabase with @Database,including entities and version.
+ * */
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideoDatabase : RoomDatabase() {
+    /**
+     * (03) Inside VideosDatabase, create abstract val videoDao.
+     * */
+    abstract val videoDao: VideoDao
+}
 
-// TODO (02) Annotate VideosDatabase with @Database,including entities and version.
+/**
+ * (04) Create an INSTANCE variable to store the VideosDatabase singleton.
+ * */
+private lateinit var INSTANCE: VideoDatabase
 
-// TODO (03) Inside VideosDatabase, create abstract val videoDao.
-
-// TODO (04) Create an INSTANCE variable to store the VideosDatabase singleton.
-
-// TODO (05) Define a function getDatabase() that returns the VideosDatabase INSTANCE.
-
-// TODO (06) Inside getDatabase(), before returning INSTANCE, use a synchronized{} block to
-// check whether INSTANCE is initialized, and, if it isn’t, use DatabaseBuilder to create it.
+/**
+ * (05) Define a function getDatabase() that returns the VideosDatabase INSTANCE.
+ * */
+fun getDatabase(context: Context): VideoDatabase {
+    /**
+     * (06) Inside getDatabase(), before returning INSTANCE, use a synchronized{} block to
+     * check whether INSTANCE is initialized, and, if it isn’t, use DatabaseBuilder to create it.
+     * */
+    synchronized(VideoDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideoDatabase::class.java,
+                "videos"
+            )
+                .build()
+        }
+    }
+    return INSTANCE
+}
